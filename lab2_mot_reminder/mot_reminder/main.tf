a7e5443f918bb8a0059508926da8ebc039e3f5b9
Gabriele Teotino <Gabriele.Teotino@opengi.co.uk>
2021-03-21 14:55:38 +0000

    Add resource groups

--- lab2_mot_reminder/mot_reminder/main.tf
+++ lab2_mot_reminder/mot_reminder/main.tf
@@ -5,3 +5,13 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg_mot" {
+  name = "rg-mot"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_mot_func" {
+  name = "rg-mot-func"
+  location = var.location
+}
